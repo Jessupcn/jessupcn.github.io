bac3c962b7c458091dbe2eed0c025dad6c08d306
Connor Jessup <cjessup@umich.edu>
2018-11-13 21:36:37 -0500

    EDIT: NavBar CSS

--- Client/Components/NavBar.js
+++ Client/Components/NavBar.js
@@ -5,12 +5,12 @@ const NavBar = () => {
     <div className="NavBar">
       <div className="NavTitle">
         <img src="../../Media/me.jpg" className="NavPic" />
-        <h3>Connor Jessup</h3>
+        <h3 className="NavBarHeader">Connor Jessup</h3>
       </div>
-      <h3>About</h3>
-      <h3>Resume</h3>
-      <h3>Projects</h3>
-      <h3>Contact</h3>
+      <h3 className="NavBarHeader">About</h3>
+      <h3 className="NavBarHeader">Resume</h3>
+      <h3 className="NavBarHeader">Projects</h3>
+      <h3 className="NavBarHeader">Contact</h3>
     </div>
   );
 };
