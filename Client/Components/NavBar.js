244d327506009fdf32e59f90775fef2895bf114c
Connor Jessup <cjessup@umich.edu>
2018-11-13 21:20:31 -0500

    ADDED: NavBar content and css

--- Client/Components/NavBar.js
+++ Client/Components/NavBar.js
@@ -3,7 +3,14 @@ import React from 'react';
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <p>I Am a lowly navbar</p>
+      <div>
+        <img src="../../Media/me.jpg" className="NavPic" />
+        <h3>Connor Jessup</h3>
+      </div>
+      <h3>About</h3>
+      <h3>Resume</h3>
+      <h3>Projects</h3>
+      <h3>Contact</h3>
     </div>
   );
 };
