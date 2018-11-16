a2c3dfe11127e649446cb9a4ae4452efe05d4860
Connor Jessup <cjessup@umich.edu>
2018-11-16 16:15:17 -0500

    ADDED: Contact component

---
+++ Client/Components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="sectionContainer contact">
+      <h1 className="sectionHeader">Contact Me</h1>
+      <div className="contactLogoContainer">
+        <div />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
