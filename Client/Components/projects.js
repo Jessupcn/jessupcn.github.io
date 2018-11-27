6fa84b479c938054e85657410e117780d1c6717a
Connor Jessup <cjessup@umich.edu>
2018-11-27 14:43:15 -0500

    ADDED: portfolio.js

---
+++ Client/Components/projects.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="sectionContainer about">
+      <h1 className="sectionHeader">Projects</h1>
+      <div className="flex-row aboutContent">
+        <div className="portfolioSection">
+          <h3>SimpleStocks</h3>
+          <div>
+            <p />
+            <img />
+          </div>
+        </div>
+        <div className="portfolioSection">
+          <h3>DevBook</h3>
+          <div>
+            <p />
+            <img />
+          </div>
+        </div>
+        <div className="portfolioSection">
+          <h3>Mr. Mixologist</h3>
+          <div>
+            <p />
+            <img />
+          </div>
+          <div>
+            <a href="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
