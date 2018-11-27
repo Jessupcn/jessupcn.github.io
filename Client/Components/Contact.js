d1b5818804f27c47b6ff345d04f1496c0a8af507
Connor Jessup <cjessup@umich.edu>
2018-11-27 14:49:59 -0500

    EDIT: contact.js

--- Client/Components/Contact.js
+++ Client/Components/Contact.js
@@ -4,13 +4,16 @@ const Contact = () => {
   return (
     <div className="sectionContainer contact">
       <h1 className="sectionHeader">Contact Me</h1>
-      <div className="contactLogoContainer">
-        <div>
-          <p>LinkedIn</p>
-        </div>
-        <div>
-          <p>github</p>
-        </div>
+      <div className="contactLogoContainer flex-row">
+        <a href="https://www.linkedin.com/in/connor-jessup/">
+          <img src="../../Media/linkedin.png" />
+        </a>
+        <a href="https://github.com/Jessupcn">
+          <img src="../../Media/github.png" />
+        </a>
+        <a href="mailto:jessupcn@gmail.com">
+          <img src="../../Media/mail.png" />
+        </a>
       </div>
     </div>
   );
