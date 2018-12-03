0fbb5d01e6d3636b0cd3eeb6ed787d9555427da0
Connor Jessup <cjessup@umich.edu>
2018-12-03 16:14:16 -0500

    attempt to fix routing issue

--- Client/history.js
+++ Client/history.js
@@ -2,6 +2,10 @@ import createHistory from 'history/createBrowserHistory';
 import createMemoryHistory from 'history/createMemoryHistory';
 
 const history =
-  process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory();
+  process.env.NODE_ENV === 'test'
+    ? createMemoryHistory()
+    : createHistory({
+        basename: process.env.PUBLIC_URL
+      });
 
 export default history;
