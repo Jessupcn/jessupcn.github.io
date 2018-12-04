3c509170ec80cf2469058ba9428af61986a67e04
Connor Jessup <cjessup@umich.edu>
2018-12-04 15:59:18 -0500

    added more to resume

--- Client/index.js
+++ Client/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import history from './history';
+// import history from './history';
 import App from './app';
 
 // give App a router
 ReactDOM.render(
-  <HashRouter history={history}>
+  <HashRouter>
     <App />
   </HashRouter>,
   document.getElementById('app')
